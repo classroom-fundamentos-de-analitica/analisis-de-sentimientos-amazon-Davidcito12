1a408cd8dec61e41f5c490400a1e56c32b456ce7
David Lopez <92931033+Davidcito12@users.noreply.github.com>
2022-11-13 19:14:58 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -72,7 +72,6 @@ def pregunta_03():
     # Retorne el analizador de palabras
     return lambda x: (stemmer.stem(w) for w in analyzer(x))
 
-
 def pregunta_04():
     """
     Especificación del pipeline y entrenamiento
@@ -80,13 +79,16 @@ def pregunta_04():
     """
 
     # Importe CountVetorizer
+    from sklearn.feature_extraction.text import CountVectorizer as Vectorizer
     # Importe GridSearchCV
+    from sklearn.model_selection import GridSearchCV as GridSearch
     # Importe Pipeline
+    from sklearn.pipeline import Pipeline
     # Importe BernoulliNB
-    from ____ import ____
+    from sklearn.naive_bayes import BernoulliNB as Bernoulli
 
     # Cargue las variables.
-    x_train, _, y_train, _ = pregunta_02()
+    x_train, useless, y_train, useless2 = pregunta_02()
 
     # Obtenga el analizador de la pregunta 3.
     analyzer = pregunta_03()
@@ -96,21 +98,21 @@ def pregunta_04():
     # límite superior para la frecuencia de palabras es del 100% y un límite
     # inferior de 5 palabras. Solo deben analizarse palabras conformadas por
     # letras.
-    countVectorizer = ____(
-        analyzer=____,
-        lowercase=____,
-        stop_words=____,
-        token_pattern=____,
-        binary=____,
-        max_df=____,
-        min_df=____,
+    countVectorizer = Vectorizer(
+        analyzer=analyzer,
+        lowercase=True,
+        stop_words="english",
+        token_pattern= r"\b\w\w+\b",
+        binary= True,
+        max_df=1.0,
+        min_df=5,
     )
 
     # Cree un pipeline que contenga el CountVectorizer y el modelo de BernoulliNB.
-    pipeline = ____(
+    pipeline = Pipeline(
         steps=[
-            ("____", ____),
-            ("____", ____()),
+            ("Vectorizer", countVectorizer),
+            ("Bernoulli", Bernoulli()),
         ],
     )
 
@@ -118,18 +120,18 @@ def pregunta_04():
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "____": np.____(____, ____, ____),
+        "Bernoulli_alpha": np.linspace(0.1, 1, 10),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y "accuracy" como métrica de evaluación
-    gridSearchCV = ____(
-        estimator=____,
-        param_grid=____,
-        cv=____,
-        scoring=____,
-        refit=____,
-        return_train_score=____,
+    gridSearchCV = GridSearch(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv= 5,
+        scoring= "accuracy",
+        refit=True,
+        return_train_score=False,
     )
 
     # Búsque la mejor combinación de regresores
