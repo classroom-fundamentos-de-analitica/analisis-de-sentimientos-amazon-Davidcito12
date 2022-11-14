39017550cc009d7e618bf2208afcfd6a568e5a10
David Lopez <92931033+Davidcito12@users.noreply.github.com>
2022-11-13 19:37:29 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -148,7 +148,7 @@ def pregunta_05():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix as Matriz
 
     # Obtenga el pipeline de la pregunta 3.
     gridSearchCV = pregunta_04()
@@ -157,14 +157,14 @@ def pregunta_05():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = Matriz(
+        y_true=y_train,
+        y_pred=gridSearchCV.predict(X_train)
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = Matriz(
+        y_true = y_test,
+        y_pred = gridSearchCV.predict(X_test)
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
